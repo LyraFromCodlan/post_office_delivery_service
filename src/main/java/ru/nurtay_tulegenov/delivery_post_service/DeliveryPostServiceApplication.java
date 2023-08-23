2a2edd273d41ad6c9e7cdd571ac3a4c0d47eba76
LyraFromCodlan <nurtaytulegenov@gmail.com>
2023-08-23 20:15:04 +0600

    initial commit

---
+++ src/main/java/ru/nurtay_tulegenov/delivery_post_service/DeliveryPostServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.nurtay_tulegenov.delivery_post_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryPostServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryPostServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nurtay_tulegenov/delivery_post_service/DeliveryPostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nurtay_tulegenov.delivery_post_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryPostServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
