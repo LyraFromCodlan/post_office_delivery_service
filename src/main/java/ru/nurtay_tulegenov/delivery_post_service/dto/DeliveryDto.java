ffdfadce096add57737713b9096d69bf6d2da864
LyraFromCodlan <nurtaytulegenov@gmail.com>
2023-09-14 12:38:13 +0600

    Added fields in DTO to send data to service

--- src/main/java/ru/nurtay_tulegenov/delivery_post_service/dto/DeliveryDto.java
+++ src/main/java/ru/nurtay_tulegenov/delivery_post_service/dto/DeliveryDto.java
@@ -1,7 +1,15 @@
 package ru.nurtay_tulegenov.delivery_post_service.dto;
 
+import jakarta.persistence.Column;
 import lombok.Data;
+import ru.nurtay_tulegenov.delivery_post_service.enums.DeliveryType;
 
 @Data
 public class DeliveryDto {
+    private String receiverIndex;
+    private String receiveAddress;
+    private String receiverName;
+    private DeliveryType deliveryType;
+
+    private String PostOfficeCode;
 }
