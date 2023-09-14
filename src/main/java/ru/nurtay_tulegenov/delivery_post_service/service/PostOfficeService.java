1566532d7958ad99d3dd270154198173e1debfaa
LyraFromCodlan <nurtaytulegenov@gmail.com>
2023-09-14 12:40:00 +0600

    Removed import of an unnecessary annotation

--- src/main/java/ru/nurtay_tulegenov/delivery_post_service/service/PostOfficeService.java
+++ src/main/java/ru/nurtay_tulegenov/delivery_post_service/service/PostOfficeService.java
@@ -1,6 +1,5 @@
 package ru.nurtay_tulegenov.delivery_post_service.service;
 
-import org.springframework.stereotype.Service;
 import ru.nurtay_tulegenov.delivery_post_service.dto.PostOfficeDto;
 import ru.nurtay_tulegenov.delivery_post_service.model.PostOffice;
 
